5f246507fd0f573407d141a0e3f061ac9b62ffc0
Marcio <mpvalenew@gmail.com>
2022-12-20 23:03:34 -0300

    Fim das alterações

--- desafio.kt
+++ desafio.kt
@@ -4,18 +4,19 @@ enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
 class Usuario
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var conteudos:) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf(Fulano, Siclano, Alcano)
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        
+        
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+val nivel = Nivel.INTERMEDIARIO
+    
 }
